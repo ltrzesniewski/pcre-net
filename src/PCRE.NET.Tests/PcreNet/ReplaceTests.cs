6458e6f9f9f0b6d55743b7ddd1af578b2cb581d3
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2014-10-16 02:04:03 +0200

    Replace function

---
+++ src/PCRE.NET.Tests/PcreNet/ReplaceTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+
+namespace PCRE.Tests.PcreNet
+{
+    [TestFixture]
+    public class ReplaceTests
+    {
+        [Test]
+        public void should_replace_matches_with_callback()
+        {
+            var re = new PcreRegex(@"a+", PcreOptions.IgnoreCase);
+            var result = re.Replace("foo aaa bar aAAa baz", match => match.Length.ToString(CultureInfo.InvariantCulture));
+
+            Assert.That(result, Is.EqualTo("foo 3 b1r 4 b1z"));
+        }
+
+        [Test]
+        public void should_replace_matches_with_pattern()
+        {
+            var re = new PcreRegex(@"a+(b+)", PcreOptions.IgnoreCase);
+            var result = re.Replace("foo aabb bar aaabbab baz", "<$0><$1>");
+
+            Assert.That(result, Is.EqualTo("foo <aabb><bb> bar <aaabb><bb><ab><b> baz"));
+        }
+    }
+}
--- src/PCRE.NET/PcreRegex.cs
+++ src/PCRE.NET/PcreRegex.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using PCRE.Support;
 using PCRE.Wrapper;
 
@@ -73,6 +74,112 @@ namespace PCRE
                 match = new PcreMatch(this, subject, offsets);
                 yield return match;
             }
+        }
+
+        public string Replace(string subject, string replacement)
+        {
+            if (replacement == null)
+                throw new ArgumentNullException("replacement");
+
+            return Replace(subject, BuildReplacementFunc(replacement));
+        }
+
+        public string Replace(string subject, Func<PcreMatch, string> replacementFunc)
+        {
+            return Replace(subject, replacementFunc, -1);
+        }
+
+        public string Replace(string subject, Func<PcreMatch, string> replacementFunc, int count)
+        {
+            if (subject == null)
+                throw new ArgumentNullException("subject");
+            if (replacementFunc == null)
+                throw new ArgumentNullException("replacementFunc");
+
+            if (count == 0)
+                return subject;
+
+            var sb = new StringBuilder((int)(subject.Length * 1.2));
+            var position = 0;
+
+            foreach (var match in Matches(subject))
+            {
+                sb.Append(subject, position, match.Index - position);
+                sb.Append(replacementFunc(match));
+                position = match.Index + match.Length;
+
+                if (--count == 0)
+                    break;
+            }
+
+            sb.Append(subject, position, subject.Length - position);
+            return sb.ToString();
+        }
+
+        private static Func<PcreMatch, string> BuildReplacementFunc(string replacementPattern)
+        {
+            var placeholderIndex = replacementPattern.IndexOf('$');
+
+            if (placeholderIndex < 0)
+                return match => replacementPattern;
+
+            var replacementParts = new List<object>();
+            var currentIndex = 0;
+
+            while (true)
+            {
+                if (placeholderIndex > currentIndex)
+                    replacementParts.Add(replacementPattern.Substring(currentIndex, placeholderIndex - currentIndex));
+
+                currentIndex = placeholderIndex + 1;
+                var endIntIndex = currentIndex;
+
+                while (endIntIndex < replacementPattern.Length)
+                {
+                    var c = replacementPattern[endIntIndex];
+                    if (c < '0' || c > '9')
+                        break;
+                    ++endIntIndex;
+                }
+
+                if (endIntIndex == currentIndex)
+                {
+                    replacementParts.Add("$");
+                }
+                else
+                {
+                    var groupIndex = Int32.Parse(replacementPattern.Substring(currentIndex, endIntIndex - currentIndex));
+                    replacementParts.Add(groupIndex);
+                    currentIndex = endIntIndex;
+                }
+
+                placeholderIndex = replacementPattern.IndexOf('$', currentIndex);
+                if (placeholderIndex < 0)
+                    break;
+            }
+
+            if (currentIndex < replacementPattern.Length)
+                replacementParts.Add(replacementPattern.Substring(currentIndex));
+
+            return match =>
+            {
+                var sb = new StringBuilder();
+
+                foreach (var item in replacementParts)
+                {
+                    var str = item as string;
+                    if (str != null)
+                    {
+                        sb.Append(str);
+                        continue;
+                    }
+
+                    var groupIndex = (int)item;
+                    sb.Append(match[groupIndex].Value);
+                }
+
+                return sb.ToString();
+            };
         }
     }
 }
