71dfa0a8c666d38a8ac615b8a196838e9e1985c6
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2014-11-16 23:13:57 +0100

    Add NuGet restore

--- build/Build.bat
+++ build/Build.bat
@@ -1,4 +1,11 @@
 @echo off
-cd /D "%~dp0"
-call ..\src\packages\psake.4.3.2\tools\psake.cmd tools\default.ps1 %*
+pushd "%~dp0"
+
+.\tools\nuget.exe restore ..\src\PCRE.NET.sln
+if errorlevel 1 goto end
+
+call ..\src\packages\psake.4.3.2\tools\psake.cmd .\tools\default.ps1 %*
+
+:end
+popd
 pause
