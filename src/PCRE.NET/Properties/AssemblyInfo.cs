639b0b50ecb9d99d0661b622d4169f6de6908952
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2019-02-16 19:32:54 +0100

    Use new project format

--- src/PCRE.NET/Properties/AssemblyInfo.cs
+++ src/PCRE.NET/Properties/AssemblyInfo.cs
@@ -1,38 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("PCRE.NET")]
-[assembly: AssemblyDescription("PCRE wrapper for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PCRE.NET")]
-[assembly: AssemblyCopyright("Copyright © Lucas Trzesniewski 2014-2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e9bfed9-e383-4589-8238-fb8a3d497f39")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.1.0")]
-[assembly: AssemblyFileVersion("0.7.1.0")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("PCRE.NET.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f155bb9aacf1d75e12075cf2dc0119341b318c8c322be0320af5d7a6ed6e24a33be604d0fbbd617092c8efb4890a4d02373b2c581ef82c5f811fb99013336e82fa3dbe4dcaea611c9d408e6143eb96ba00eaf8623b54dfea69160157df1cce3b86cd940d526c7ecf779651b896040ea391849732c06193593e5dcbbf514b24a2")]
