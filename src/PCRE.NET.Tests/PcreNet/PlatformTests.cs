aec61ec584b0c66b3376b72ac680f928871cf5a0
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2019-02-17 23:32:50 +0100

    Uncomment remaining tests

--- src/PCRE.NET.Tests/PcreNet/PlatformTests.cs
+++ src/PCRE.NET.Tests/PcreNet/PlatformTests.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using NUnit.Framework;
-using PCRE.Internal;
 
 namespace PCRE.Tests.PcreNet
 {
@@ -14,18 +10,6 @@ namespace PCRE.Tests.PcreNet
         public void print_current_platform()
         {
             Console.WriteLine("TESTS RUNNING IN {0}-bit mode", Environment.Is64BitProcess ? 64 : 32);
-        }
-
-        [Test, Explicit]
-        public void print_asmz_hashes()
-        {
-            foreach (var prefix in new[] { "", "x86:", "x64:" })
-            {
-                var str = prefix + "PCRE.NET.Wrapper, Version=0.0.0.0, Culture=neutral, PublicKeyToken=8f58d558eeff25a3";
-                var hash = new Guid(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(str.ToLowerInvariant())));
-
-                Console.WriteLine("{0:N} = {1}", hash, str);
-            }
         }
 
         [Test]
