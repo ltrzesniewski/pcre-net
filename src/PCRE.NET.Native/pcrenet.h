d019b13467b69d263ff83931e7f94e0de3059338
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2019-02-26 15:50:59 +0100

    Add CMakeLists.txt

--- src/PCRE.NET.Native/pcrenet.h
+++ src/PCRE.NET.Native/pcrenet.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdint>
+
+#include "../PCRE/src/config.h"
 #include "../PCRE/src/pcre2.h"
 
 #define PCRENET_EXPORT(type, name) extern "C" __declspec(dllexport) type __stdcall pcrenet_##name
