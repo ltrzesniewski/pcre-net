855bd5e112080be38c1c8cf9eff6cb2dc1a7e11f
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2021-10-17 15:44:07 +0200

    Document build and pattern info

--- src/PCRE.NET/PcreBuildInfo.cs
+++ src/PCRE.NET/PcreBuildInfo.cs
@@ -1,25 +1,96 @@
-﻿using System;
-using PCRE.Internal;
+﻿using PCRE.Internal;
 
 namespace PCRE
 {
+    /// <summary>
+    /// Returns information about the PCRE build.
+    /// </summary>
     public static unsafe class PcreBuildInfo
     {
+        /// <summary>
+        /// <c>PCRE2_CONFIG_BSR</c> - Indicates what character sequences the <c>\R</c> escape sequence matches by default.
+        /// </summary>
+        /// <remarks>
+        /// The default can be overridden when a pattern is compiled.
+        /// </remarks>
         public static PcreBackslashR BackslashR { get; } = (PcreBackslashR)GetConfigUInt32(PcreConstants.CONFIG_BSR);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_JIT</c> - Indicates if support for just-in-time compiling is available.
+        /// </summary>
         public static bool Jit { get; } = GetConfigBool(PcreConstants.CONFIG_JIT);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_JITTARGET</c> - Returns a string that contains the name of the architecture for which the JIT compiler is configured.
+        /// </summary>
         public static string JitTarget { get; } = GetConfigString(PcreConstants.CONFIG_JITTARGET);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_LINKSIZE</c> - Indicates the number of bytes used for internal linkage in compiled regular expressions.
+        /// </summary>
         public static uint LinkSize { get; } = GetConfigUInt32(PcreConstants.CONFIG_LINKSIZE);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_MATCHLIMIT</c> - Returns the default match limit.
+        /// </summary>
         public static uint MatchLimit { get; } = GetConfigUInt32(PcreConstants.CONFIG_MATCHLIMIT);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_NEWLINE</c> - Indicates the default character sequence that is recognized as meaning "newline".
+        /// </summary>
         public static PcreNewLine NewLine { get; } = (PcreNewLine)GetConfigUInt32(PcreConstants.CONFIG_NEWLINE);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_PARENSLIMIT</c> - Returns the maximum depth of nesting of parentheses (of any kind) in a pattern.
+        /// </summary>
+        /// <remarks>
+        /// This limit is imposed to cap the amount of system stack used when a pattern is compiled.
+        /// </remarks>
         public static uint ParensLimit { get; } = GetConfigUInt32(PcreConstants.CONFIG_PARENSLIMIT);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_DEPTHLIMIT</c> - Returns the default limit for the depth of nested backtracking in NFA matching or the depth of nested recursions, lookarounds, and atomic groups in DFA matching.
+        /// </summary>
         public static uint DepthLimit { get; } = GetConfigUInt32(PcreConstants.CONFIG_DEPTHLIMIT);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_UNICODE</c> - Indicates if Unicode support is available.
+        /// </summary>
         public static bool Unicode { get; } = GetConfigBool(PcreConstants.CONFIG_UNICODE);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_UNICODE_VERSION</c> - Returns the supported Unicode version.
+        /// </summary>
         public static string UnicodeVersion { get; } = GetConfigString(PcreConstants.CONFIG_UNICODE_VERSION);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_VERSION</c> - Returns the PCRE2 version string.
+        /// </summary>
         public static string Version { get; } = GetConfigString(PcreConstants.CONFIG_VERSION);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_HEAPLIMIT</c> - Returns the default limit for the amount of heap memory used for matching, in kibibytes.
+        /// </summary>
         public static uint HeapLimit { get; } = GetConfigUInt32(PcreConstants.CONFIG_HEAPLIMIT);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_NEVER_BACKSLASH_C</c> - Indicates if the use of <c>\C</c> was permanently disabled when PCRE2 was built
+        /// </summary>
         public static bool NeverBackslashC { get; } = GetConfigBool(PcreConstants.CONFIG_NEVER_BACKSLASH_C);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_COMPILED_WIDTHS</c> - Indicates which code unit widths were selected when PCRE2 was built.
+        /// </summary>
+        /// <remarks>
+        /// The 1-bit indicates 8-bit support, and the 2-bit and 4-bit indicate 16-bit and 32-bit support, respectively.
+        /// </remarks>
         public static uint CompiledWidths { get; } = GetConfigUInt32(PcreConstants.CONFIG_COMPILED_WIDTHS);
+
+        /// <summary>
+        /// <c>PCRE2_CONFIG_TABLES_LENGTH</c> - Gives the length of PCRE2's character processing tables in bytes.
+        /// </summary>
         public static uint TablesLength { get; } = GetConfigUInt32(PcreConstants.CONFIG_TABLES_LENGTH);
+
 
         private static bool GetConfigBool(uint key)
             => GetConfigUInt32(key) != 0;
--- src/PCRE.NET/PcreJitStack.cs
+++ src/PCRE.NET/PcreJitStack.cs
@@ -8,13 +8,29 @@ namespace PCRE
     /// JIT stack for advanced usage scenarios.
     /// </summary>
     /// <remarks>
+    /// <para>
     /// Not thread-safe and not reentrant.
+    /// </para>
+    /// <para>
+    /// When the compiled JIT code runs, it needs a block of memory to use as a stack. By default, it uses 32KiB on the machine stack.
+    /// However, some large or complicated patterns need more than this. The error <see cref="PcreErrorCode.JitStackLimit"/> is given when there is not enough stack.
+    /// </para>
+    /// <para>
+    /// You may safely use the same JIT stack for more than one pattern (either by assigning directly or by callback), as long as the patterns are matched sequentially in the same thread.
+    /// Currently, the only way to set up non-sequential matches in one thread is to use callouts: if a callout function starts another match,
+    /// that match must use a different JIT stack to the one used for currently suspended match(es).
+    /// </para>
     /// </remarks>
     [SuppressMessage("Naming", "CA1711")]
     public sealed class PcreJitStack : IDisposable
     {
         private IntPtr _stack;
 
+        /// <summary>
+        /// Creates a JIT stack.
+        /// </summary>
+        /// <param name="startSize">The initial stack size.</param>
+        /// <param name="maxSize">The maximum stack size.</param>
         public PcreJitStack(uint startSize, uint maxSize)
         {
             _stack = Native.jit_stack_create(startSize, maxSize);
@@ -25,6 +41,9 @@ namespace PCRE
             FreeStack();
         }
 
+        /// <summary>
+        /// Disposes the JIT stack.
+        /// </summary>
         public void Dispose()
         {
             FreeStack();
