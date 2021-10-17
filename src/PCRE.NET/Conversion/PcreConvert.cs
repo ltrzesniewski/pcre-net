c8e8e5f6cac695ec7bd918591166a326b7c4e562
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2021-10-17 18:42:17 +0200

    Document conversion functions

--- src/PCRE.NET/Conversion/PcreConvert.cs
+++ src/PCRE.NET/Conversion/PcreConvert.cs
@@ -3,14 +3,34 @@ using PCRE.Internal;
 
 namespace PCRE.Conversion
 {
+    /// <summary>
+    /// Pattern conversions.
+    /// </summary>
     public static unsafe class PcreConvert
     {
+        /// <summary>
+        /// Converts a POSIX basic pattern to a PCRE pattern.
+        /// </summary>
+        /// <param name="pattern">The pattern to convert.</param>
         public static string FromPosixBasic(string pattern)
             => BasicConvert(pattern, PcreConstants.CONVERT_POSIX_BASIC);
 
+        /// <summary>
+        /// Converts a POSIX extended pattern to a PCRE pattern.
+        /// </summary>
+        /// <param name="pattern">The pattern to convert.</param>
         public static string FromPosixExtended(string pattern)
             => BasicConvert(pattern, PcreConstants.CONVERT_POSIX_EXTENDED);
 
+        /// <summary>
+        /// Converts a glob pattern to a PCRE pattern.
+        /// </summary>
+        /// <param name="pattern">The pattern to convert.</param>
+        /// <param name="options">Conversion options.</param>
+        /// <remarks>
+        /// Globs are used to match file names, and consequently have the concept of a "path separator", which defaults to backslash under Windows and forward slash otherwise.
+        /// The wildcards <c>*</c> and <c>?</c> are not permitted to match separator characters, but the double-star (<c>**</c>) feature (which does match separators) is supported.
+        /// </remarks>
         public static string FromGlob(string pattern, PcreGlobConversionOptions options)
         {
             if (pattern == null) throw new ArgumentNullException(nameof(pattern));
--- src/PCRE.NET/Conversion/PcreGlobConversionOptions.cs
+++ src/PCRE.NET/Conversion/PcreGlobConversionOptions.cs
@@ -1,23 +1,56 @@
-﻿using PCRE.Internal;
+﻿using System.Runtime.InteropServices;
+using PCRE.Internal;
 
 namespace PCRE.Conversion
 {
+    /// <summary>
+    /// Conversion options for glob patterns.
+    /// </summary>
     public sealed class PcreGlobConversionOptions
     {
+        /// <summary>
+        /// <c>PCRE2_CONVERT_GLOB_NO_WILD_SEPARATOR</c> - Allow wildcards to match separator characters.
+        /// </summary>
         public bool NoWildcardSeparator { get; set; }
+
+        /// <summary>
+        /// <c>PCRE2_CONVERT_GLOB_NO_STARSTAR</c> - Disable the double-star (<c>**</c>) wildcard.
+        /// </summary>
         public bool NoStarStar { get; set; }
-        public char EscapeCharacter { get; set; } = '`';
-        public char SeparatorCharacter { get; set; } = '\\';
 
-        public static PcreGlobConversionOptions DefaultWindows()
-            => new PcreGlobConversionOptions();
+        /// <summary>
+        /// The escape character.
+        /// </summary>
+        /// <remarks>
+        /// Defaults to <c>`</c> under Windows, and <c>\</c> otherwise.
+        /// </remarks>
+        public char EscapeCharacter { get; set; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? '`' : '\\';
 
-        public static PcreGlobConversionOptions DefaultUnix()
-            => new PcreGlobConversionOptions
-            {
-                EscapeCharacter = '\\',
-                SeparatorCharacter = '/'
-            };
+        /// <summary>
+        /// The path separator character.
+        /// </summary>
+        /// <remarks>
+        /// Defaults to <c>\</c> under Windows, and <c>/</c> otherwise.
+        /// </remarks>
+        public char SeparatorCharacter { get; set; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? '\\' : '/';
+
+        /// <summary>
+        /// Returns default options for Windows.
+        /// </summary>
+        public static PcreGlobConversionOptions DefaultWindows() => new()
+        {
+            EscapeCharacter = '`',
+            SeparatorCharacter = '\\'
+        };
+
+        /// <summary>
+        /// Returns default options for Unix.
+        /// </summary>
+        public static PcreGlobConversionOptions DefaultUnix() => new()
+        {
+            EscapeCharacter = '\\',
+            SeparatorCharacter = '/'
+        };
 
         internal void FillConvertInput(ref Native.convert_input input)
         {
