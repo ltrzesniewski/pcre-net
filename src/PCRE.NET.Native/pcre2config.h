a6f950fcae3a017b393249c362ba87379a612b69
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2024-12-27 20:54:09 +0100

    Update build config

--- src/PCRE.NET.Native/pcre2config.h
+++ src/PCRE.NET.Native/pcre2config.h
@@ -6,7 +6,6 @@
 /* ReSharper disable All */
 
 #define HAVE_ASSERT_H 1
-#define HAVE_BUILTIN_ASSUME
 #define HAVE_INTTYPES_H 1
 #define HAVE_LIMITS_H 1
 #define HAVE_MEMMOVE 1
@@ -17,7 +16,6 @@
 #define HAVE_SYS_STAT_H 1
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_WCHAR_H 1
-#define HAVE_WINDOWS_H 1
 
 #define NEWLINE_DEFAULT 5
 
@@ -26,4 +24,24 @@
 #define SUPPORT_JIT 1
 #define SUPPORT_UNICODE 1
 
+#if _MSC_VER
+
+#define HAVE_BUILTIN_ASSUME
+#define HAVE_WINDOWS_H 1
 #define _CRT_SECURE_NO_WARNINGS
+
+#elif __GNUC__
+
+#define HAVE_BUILTIN_MUL_OVERFLOW
+#define HAVE_BUILTIN_UNREACHABLE
+
+#elif __clang__
+
+#define HAVE_ATTRIBUTE_UNINITIALIZED
+#define HAVE_BUILTIN_UNREACHABLE
+
+#else
+
+#error "Unexpected compiler"
+
+#endif
