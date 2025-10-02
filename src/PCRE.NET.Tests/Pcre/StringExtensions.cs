f52de55fbb479879a410306b280f3cb7a8d59bc9
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2025-10-02 21:32:26 +0200

    Use extension blocks

--- src/PCRE.NET.Tests/Pcre/StringExtensions.cs
+++ src/PCRE.NET.Tests/Pcre/StringExtensions.cs
@@ -9,134 +9,137 @@ public static class StringExtensions
     // Taken from perltest.sh, see https://github.com/PCRE2Project/pcre2/pull/529
     private static readonly Regex _hexRe = new("""\s*(?:([0-9a-fA-F]{2})|(['"])([^\2]+?)\2)\s*""");
 
-    public static string Escape(this string str)
+    extension(string str)
     {
-        var sb = new StringBuilder();
-
-        foreach (var c in str)
+        public string Escape()
         {
-            if (c == '\\')
-                sb.Append(@"\\");
-            else if (c < 32 || c > 126)
-                sb.AppendFormat(@"\x{0:X2}", (short)c);
-            else
-                sb.Append(c);
+            var sb = new StringBuilder();
+
+            foreach (var c in str)
+            {
+                if (c == '\\')
+                    sb.Append(@"\\");
+                else if (c < 32 || c > 126)
+                    sb.Append($@"\x{(short)c:X2}");
+                else
+                    sb.Append(c);
+            }
+
+            return sb.ToString();
         }
 
-        return sb.ToString();
-    }
-
-    public static string UnescapeSubject(this string str)
-    {
-        var sb = new StringBuilder();
-
-        for (var i = 0; i < str.Length; ++i)
+        public string UnescapeSubject()
         {
-            if (str[i] == '\\')
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < str.Length; ++i)
             {
-                ++i;
-                if (i >= str.Length)
-                    break;
-
-                switch (str[i])
+                if (str[i] == '\\')
                 {
-                    case '\\':
-                        sb.Append('\\');
-                        break;
-                    case 't':
-                        sb.Append('\t');
-                        break;
-                    case 'n':
-                        sb.Append('\n');
-                        break;
-                    case 'r':
-                        sb.Append('\r');
-                        break;
-                    case 'f':
-                        sb.Append('\f');
-                        break;
-                    case 'a':
-                        sb.Append('\a');
-                        break;
-                    case 'e':
-                        sb.Append('\x1B');
+                    ++i;
+                    if (i >= str.Length)
                         break;
 
-                    case 'x':
-                        UnescapeHex(str, sb, ref i);
-                        break;
-
-                    case '0':
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
+                    switch (str[i])
                     {
-                        var endIndex = i;
-                        while (endIndex < str.Length && endIndex - i < 3 && str[endIndex] >= '0' && str[endIndex] <= '7')
-                            ++endIndex;
+                        case '\\':
+                            sb.Append('\\');
+                            break;
+                        case 't':
+                            sb.Append('\t');
+                            break;
+                        case 'n':
+                            sb.Append('\n');
+                            break;
+                        case 'r':
+                            sb.Append('\r');
+                            break;
+                        case 'f':
+                            sb.Append('\f');
+                            break;
+                        case 'a':
+                            sb.Append('\a');
+                            break;
+                        case 'e':
+                            sb.Append('\x1B');
+                            break;
 
-                        sb.Append((char)Convert.ToUInt16(str.Substring(i, endIndex - i), 8));
-                        i += endIndex - i - 1;
+                        case 'x':
+                            UnescapeHex(str, sb, ref i);
+                            break;
+
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        {
+                            var endIndex = i;
+                            while (endIndex < str.Length && endIndex - i < 3 && str[endIndex] >= '0' && str[endIndex] <= '7')
+                                ++endIndex;
+
+                            sb.Append((char)Convert.ToUInt16(str.Substring(i, endIndex - i), 8));
+                            i += endIndex - i - 1;
+                            break;
+                        }
+
+                        default:
+                            sb.Append(str[i]);
+                            break;
+                    }
+                }
+                else
+                {
+                    sb.Append(str[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public string UnescapeBinaryString()
+        {
+            return _hexRe.Replace(str.UnescapeSubject(), m =>
+            {
+                if (m.Groups[1].Success)
+                    return ((char)Convert.ToInt16(m.Groups[1].Value, 16)).ToString();
+
+                return m.Groups[3].Value;
+            });
+        }
+
+        public string UnescapeGroup()
+        {
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < str.Length; ++i)
+            {
+                if (str[i] == '\\')
+                {
+                    ++i;
+                    if (i >= str.Length)
+                    {
+                        sb.Append('\\');
                         break;
                     }
 
-                    default:
-                        sb.Append(str[i]);
-                        break;
+                    if (str[i] == 'x')
+                    {
+                        UnescapeHex(str, sb, ref i);
+                        continue;
+                    }
+
+                    sb.Append('\\');
                 }
-            }
-            else
-            {
+
                 sb.Append(str[i]);
             }
+
+            return sb.ToString();
         }
-
-        return sb.ToString();
-    }
-
-    public static string UnescapeBinaryString(this string str)
-    {
-        return _hexRe.Replace(str.UnescapeSubject(), m =>
-        {
-            if (m.Groups[1].Success)
-                return ((char)Convert.ToInt16(m.Groups[1].Value, 16)).ToString();
-
-            return m.Groups[3].Value;
-        });
-    }
-
-    public static string UnescapeGroup(this string str)
-    {
-        var sb = new StringBuilder();
-
-        for (var i = 0; i < str.Length; ++i)
-        {
-            if (str[i] == '\\')
-            {
-                ++i;
-                if (i >= str.Length)
-                {
-                    sb.Append('\\');
-                    break;
-                }
-
-                if (str[i] == 'x')
-                {
-                    UnescapeHex(str, sb, ref i);
-                    continue;
-                }
-
-                sb.Append('\\');
-            }
-
-            sb.Append(str[i]);
-        }
-
-        return sb.ToString();
     }
 
     private static void UnescapeHex(string str, StringBuilder sb, ref int i)
@@ -159,7 +162,7 @@ public static class StringExtensions
         {
             var c = str[i + 2];
 
-            if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))
+            if (c is >= '0' and <= '9' or >= 'a' and <= 'f' or >= 'A' and <= 'F')
             {
                 sb.Append((char)Convert.ToUInt16(str.Substring(i + 1, 2), 16));
                 i += 2;
