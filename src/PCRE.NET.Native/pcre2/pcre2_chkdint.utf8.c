36e17b676f310a8cfd47223f2759a4a9bcf3660a
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2025-10-11 19:37:52 +0200

    Add UTF-8 variant

---
+++ src/PCRE.NET.Native/pcre2/pcre2_chkdint.utf8.c
@@ -0,0 +1,5 @@
+
+#define PCRE2_CODE_UNIT_WIDTH 8
+
+// That would be defined twice
+// #include "../../PCRE/src/pcre2_chkdint.c"
---
+++ src/PCRE.NET.Native/pcre2/pcre2_xclass.utf8.c
@@ -0,0 +1,4 @@
+
+#define PCRE2_CODE_UNIT_WIDTH 8
+
+#include "../../PCRE/src/pcre2_xclass.c"
